95a44e00aacce22b2622cf39fda6a89e3fa44f78
ALEX MOLL <alexmoll@ALEXs-MacBook-Pro.local>
2017-11-08 10:17:00 -0700

    new updates

--- public/main.js
+++ public/main.js
@@ -1,40 +1,47 @@
 $(document).ready(function() {
   console.log("ready!");
 });
-
+//any code inside doc ready wont run until html is loaded on page,
+//so best practice is to place whatever relates to html inside document ready
+//not like above
   $("#form").submit(function(event) {
     event.preventDefault();
     var sD = document.getElementById("startDate").value;
     var eD = document.getElementById("endDate").value;
-    console.log(sD)
-    console.log(eD)
+    console.log(sD);
+    console.log(eD);
+
+    //new form
+    var message = document.getElementById("message").value;
+    var speaker = document.getElementById("speaker").value;
+    $.get(`params/firstparam/${message}/secondparam/${speaker}`)
 
     //send start and end dates to the server
     $.get(`/api?start_date=${sD}&end_date=${eD}`, function(body, status) {
       //console.log(body)
       body = JSON.parse(body);
-      console.log(body)
+      console.log(body);
 
       var hazardous = []
       for (var day in body.near_earth_objects) {
         //populate all hazardous asteroids into an array
         for (var i = 0; i < body.near_earth_objects[day].length; i++) {
           if (body.near_earth_objects[day][i].is_potentially_hazardous_asteroid) {
-            hazardous.push(body.near_earth_objects[day][i])
+            hazardous.push(body.near_earth_objects[day][i]);
           }
         }
       }
       //sort largest asteroids first by max diameter
       hazardous.sort(function(a, b) {
-        return b.estimated_diameter.feet.estimated_diameter_max - a.estimated_diameter.feet.estimated_diameter_max
+        return b.estimated_diameter.feet.estimated_diameter_max - a.estimated_diameter.feet.estimated_diameter_max;
       })
 
       for (var i in hazardous) {
         // for loop around hazardous .append hazardous.name ...
         $(".theScreen").append("<br><p>Asteroid Name: " + hazardous[i].name + "</p>")
-        $(".theScreen").append("<p>Velocity MPH: " + Math.round(hazardous[i].close_approach_data[0].relative_velocity.miles_per_hour) + "</p>")
-        $(".theScreen").append("<p>Max Diameter Feet: " + Math.round(hazardous[i].estimated_diameter.feet.estimated_diameter_max) + "</p>")
-        $(".theScreen").append("<p>Distance from Earth in Miles: " + hazardous[i].close_approach_data[0].miss_distance.miles + "</p>")
+        $(".theScreen").append("<p>Velocity MPH: " + Math.round(hazardous[i].close_approach_data[0].relative_velocity.miles_per_hour) + "</p>");
+        $(".theScreen").append("<p>Max Diameter Feet: " + Math.round(hazardous[i].estimated_diameter.feet.estimated_diameter_max) + "</p>");
+        $(".theScreen").append("<p>Distance from Earth in Miles: " + hazardous[i].close_approach_data[0].miss_distance.miles + "</p>");
       }
 
     });
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 
 // for every file inside this folder, makes a app.get route
+// also for every request to server, check if request URL resembles file path in ./public
+//if it does, res.sendFile that file, if not, call next()
 app.use(express.static('./public'))
 
 //var nasaUrl= "https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-10-30&end_date=2017-10-31&api_key=nxDNl3KDNp3hoakhRWDKErj3kIbrw8SS5Y7UsCQi";
@@ -10,44 +12,47 @@ const request = require('request');
 
 //req.query is query stream coming from the client
 app.get('/api', function(req, res) {
-  console.log(req.query)
+  console.log(req.query);
   var nasaUrl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.start_date}&end_date=${req.query.end_date}&api_key=nxDNl3KDNp3hoakhRWDKErj3kIbrw8SS5Y7UsCQi`;
   request(nasaUrl, function(err, response, body) {
     console.log("started");
     //console.log(body)
-    res.send(body)
+    res.send(body);
   })
 })
 
 app.get('/', function(request, response) {
   response.sendFile('./public/index.html', {root: './'})
-  console.log("Welcome to the internet!")
+  console.log("Welcome to the internet!");
+})
+  //localhost:8080?message='hello'
+  //$.get('/?message=hello')
+  //req.query.message='hello'
+  //express turns this into an object
+  app.get('params/firstparam/:message/secondparam/:speaker', function(req,res)) {
+  console.log('query: ' + req.query)
+  console.log('params' + req.params)
+  res.send('${req.params.speaker}' says '${req.params.message}')
 })
 
-app.use(express.static('public'))
-
 app.get('/hello', function(request, response) {
-  response.send("Hello World")
+  response.send("Hello World");
 })
 
 app.get('/Home', function(request, response) {
-  response.sendFile('./public/index.html', {
-    root: './'
-  })
+  response.sendFile('./public/index.html', {root: './'})
 })
 
 app.get('/foo/bar', function(request, response) {
-  response.sendFile('./public/main.css', {
-    root: './'
-  })
+  response.sendFile('./public/main.css', {root: './'})
 })
 
 app.post('/form-submit', function(request, response) {
   console.log("received post request!")
   //response.send("received the post request!")
-  response.redirect('/about')
+  response.redirect('/about');
 })
 
 app.listen(8080, function() {
-  console.log('The app is running on 8080')
+  console.log('The app is running on 8080');
 })
